9b4877786724b09aeae70b0aa482d1814086fe99
Dritriy Golyshev <4dimariK@gmail.com>
2023-06-02 20:27:12 +0300

    feat: add data object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './index.scss';
 
-console.log('app start 111');
+const data = {
+  summer: {
+    bgUrl: '/assets/summer-bg.jpg',
+    iconSrc: '/assets/icons/sun.svg',
+    audioSrc: '/assets/sounds/summer.mp3',
+  },
+  rainy: {
+    bgUrl: '/assets/rainy-bg.jpg',
+    iconSrc: '/assets/icons/cloud-rain.svg',
+    audioSrc: '/assets/sounds/rain.mp3',
+  },
+  winter: {
+    bgUrl: '/assets/winter-bg.jpg',
+    iconSrc: '/assets/icons/cloud-snow.svg',
+    audioSrc: '/assets/sounds/winter.mp3',
+  },
+};
