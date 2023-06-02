ba5ac78de4b79bfbba85f886ff5d594ea3b8862e
Dritriy Golyshev <4dimariK@gmail.com>
2023-06-02 08:30:37 +0300

    build: move assets folder to src

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -18,7 +18,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'public/assets'),
+          from: path.resolve(__dirname, 'src/assets'),
           to: path.resolve(__dirname, 'dist/assets'),
         },
         {
